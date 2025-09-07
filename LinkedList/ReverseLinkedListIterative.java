b2582605b56d41ab9ba62b72a074380d6980d85d
LaliTYogi2002 <91830326+LaliTYogi2002@users.noreply.github.com>
2025-09-07 18:12:14 +0530

    Done Reverse Linked List Interative on leetcode

---
+++ LinkedList/ReverseLinkedListIterative.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+public class ReverseLinkedListIterative {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
+
+    }
+}
